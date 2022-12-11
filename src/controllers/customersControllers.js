be3ff19753b362dfa6ab71177a7ef19e54ef0481
isabellexvr <isabellexavier59@gmail.com>
2022-12-11 01:45:04 -0200

    feat: attCustomer route + controller

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -1,3 +1,4 @@
+import { func } from "joi";
 import { connectionDB } from "../database/db.js";
 
 export async function getCostumers(req, res) {
@@ -36,10 +37,27 @@ export async function getCostumerById(req, res) {
 }
 
 export async function postCustomer(req, res) {
-    const {name, phone, cpf, birthday} = req.body;
+  const { name, phone, cpf, birthday } = req.body;
   try {
-    await connectionDB.query("INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)", [name, phone, cpf, birthday]);
-    res.status(201).send("Cliente criado com sucesso!")
+    await connectionDB.query(
+      "INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);",
+      [name, phone, cpf, birthday]
+    );
+    res.status(201).send("Cliente criado com sucesso!");
+  } catch (err) {
+    res.status(500).send(err.message);
+    console.log(err.message);
+  }
+}
+
+export async function attCustomer(res, res) {
+  const { name, phone, cpf, birthday } = req.body;
+  const { id } = req.params;
+  try {
+    await connectionDB.query(
+      "UPDATE costumers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;",
+      [name, phone, cpf, birthday, id]
+    );
   } catch (err) {
     res.status(500).send(err.message);
     console.log(err.message);
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -3,12 +3,20 @@ import {
   getCostumers,
   getCostumerById,
   postCustomer,
+  attCustomer,
 } from "../controllers/customersControllers.js";
 import customerValidation from "../middlewares/customerValidationMiddleware.js";
 
 const customersRouter = Router();
+
 customersRouter.get("/customers", getCostumers);
+
 customersRouter.get("/customers/:id", getCostumerById);
-customersRouter.post("/customers", customerValidation, postCustomer);
+
+customersRouter.use(customerValidation);
+
+customersRouter.post("/customers", postCustomer);
+
+customersRouter.put("/customers/:id", attCustomer);
 
 export default customersRouter;
