3bf7138de3c7a64d3c78932e76eae227f15d19ab
isabellexvr <isabellexavier59@gmail.com>
2022-12-12 17:30:15 -0200

    docs: cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import categoriesRouter from "./routes/categoriesRoutes.js";
 import gamesRouter from "./routes/gamesRoutes.js";
 import customersRouter from "./routes/customersRoutes.js";
 import rentalsRouter from "./routes/rentalsRoutes.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(rentalsRouter);
 app.use(categoriesRouter);
